ce1a23e2014e5774eed29391e5b5302d81b6ef6c
Your Name <juan.pablo982206@gmail.com>
2022-01-13 18:12:45 -0500

    Creating circles with the viewset validating that a user is the owner of it

--- cride/circles/serializers/circles.py
+++ cride/circles/serializers/circles.py
@@ -6,8 +6,17 @@ from rest_framework import serializers
 # Models
 from cride.circles.models import Circle
 
+from operator import xor
+
 class CircleModelSerializer(serializers.ModelSerializer):
     """Circle model serializer"""
+
+    members_limit = serializers.IntegerField(
+        required=False,
+        min_value=10,
+        max_value=32000
+    )
+    is_limited = serializers.BooleanField(default=False)
 
     class Meta:
         """Meta class"""
@@ -18,3 +27,17 @@ class CircleModelSerializer(serializers.ModelSerializer):
             'rides_offered', 'rides_taken', 'verified',
             'is_public', 'is_limited', 'members_limit'
         )
+        read_only_fields = (
+            'is_public',
+            'verified',
+            'rides_offered',
+            'rides_taken'
+        )
+
+    def validate(self, data):
+        """Ensure both members_limit and is_limited are present"""
+        members_limit = data.get('members_limit', None)
+        is_limited = data.get('is_limited', False)
+        if is_limited ^ bool(members_limit):
+            raise serializers.ValidationError('If circle is limited, a member limit must be provided')
+        return data
