44c4572a2dca6fa59e7f382cc9422e17808f90f0
Juan Pablo Muñoz <juan.pablo982206@gmail.com>
2022-01-20 19:22:35 -0500

    Membership viewset and serializers

---
+++ cride/circles/serializers/memberships.py
@@ -0,0 +1,38 @@
+"""Membership serializers"""
+
+# DRF
+from rest_framework import serializers
+
+# Models
+from cride.circles.models import Membership
+
+# Serializers
+from cride.users.serializers import UserModelSerializer
+
+
+class MembershipModelSerializer(serializers.ModelSerializer):
+    """Member model serializer"""
+
+    user = UserModelSerializer(read_only=True)
+    joined_at = serializers.DateTimeField(source='created', read_only=True)
+    invited_by = serializers.StringRelatedField()
+
+    class Meta:
+        """Meta class"""
+
+        model = Membership
+        fields = (
+            'user',
+            'is_admin', 'is_active',
+            'used_invitations', 'remaining_invitations',
+            'invited_by',
+            'rides_taken',
+            'rides_offered', 'joined_at'
+        )
+        read_only = (
+            'user',
+            'used_invitations',
+            'invited_by',
+            'rides_taken'
+            'rides_offered'
+        )
